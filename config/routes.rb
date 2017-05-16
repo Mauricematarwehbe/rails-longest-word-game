7628222a6a316fd8e6d2ca1ee03cf6a2710485e5
Maurice Matar Wehbe <mauricematarwehbe93@gmail.com>
2017-05-16 03:20:23 -0700

    game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/game' => 'longest_word#game', as: 'game'
+
+  get '/score' => 'longest_word#score', as: 'score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
